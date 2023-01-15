340d72bed4412ee0376d281eb5bb532c1d0ae4b2
yoyuk <yoyuk@mail.ru>
2023-01-15 18:30:15 +0400

    refactor: update files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+        const newArray = String(n).split('');
+        const reverseArray = newArray.reverse();
+        const reverseString = reverseArray.toString();
+        const result = reverseString.replace(/,/g, '');
+        const reverseNumber = Number(result);
+        return reverseNumber;
+       } else {
+        const newArray = String(n).split('');
+        newArray.splice(0, 1);
+        const reverseArray = newArray.reverse();
+        const reverseString = reverseArray.toString();
+        const result = reverseString.replace(/,/g, '');
+        const reverseNumber = Number(result);
+        return reverseNumber;
+       }
 }
